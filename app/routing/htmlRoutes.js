0e98e242ae1a0e29cfb9ca0e173bac417f39cc7d
w-j-gannon <wgannon@austin.utexas.edu>
2019-03-23 21:35:39 -0500

    add * route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,7 +7,12 @@ module.exports = function(req, res) {
     })
 
     // home page
-    app.use(function(req, res) {
+    app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "..public/home.html"));
     })
+
+    // default
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 }
