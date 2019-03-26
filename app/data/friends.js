1392545339da7243edb730b708693fd21543f8c7
w-j-gannon <wgannon@austin.utexas.edu>
2019-03-25 20:37:17 -0500

    quiz update

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,43 +4,43 @@ var friends = [
         photo: "https://vignette.wikia.nocookie.net/seinfeld/images/5/55/340px-Jerryseinfe.jpg/revision/latest/scale-to-width-down/250?cb=20120427201915",
         scores: [
             5,
+            2,
+            4,
+            2,
+            2,
+            3,
+            1,
             5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5
+            3,
+            3
         ]
     }, {
         name: "Elaine Benes",
         photo: "https://vignette.wikia.nocookie.net/seinfeld/images/5/57/Elaine.jpg/revision/latest/scale-to-width-down/250?cb=20110915222502",
         scores: [
+            3,
+            3,
+            3,
+            5,
+            3,
             4,
+            2,
+            2,
             4,
-            4,
-            4,
-            4,
-            4,
-            4,
-            4,
-            4,
-            4
+            5
         ]
     }, {
         name: "George Costanza",
         photo: "https://vignette.wikia.nocookie.net/seinfeld/images/7/76/George-costanza.jpg/revision/latest?cb=20110406222711",
         scores: [
+            4,
+            5,
             1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
+            4,
+            4,
+            5,
+            4,
+            4,
             1,
             1
         ]
@@ -49,30 +49,30 @@ var friends = [
         photo: "https://vignette.wikia.nocookie.net/seinfeld/images/3/3c/Kramer.jpg/revision/latest/scale-to-width-down/250?cb=20110401134023",
         scores: [
             2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2
+            1,
+            5,
+            1,
+            1,
+            1,
+            3,
+            1,
+            5,
+            4
         ]
     }, {
         name: "Newman",
         photo: "https://vignette.wikia.nocookie.net/seinfeld/images/7/7f/Untitled.jpg/revision/latest?cb=20120128125849",
         scores: [
+            1,
+            4,
+            2,
             3,
+            5,
+            2,
+            5,
             3,
-            3,
-            3,
-            3,
-            3,
-            3,
-            3,
-            3,
-            3
+            2,
+            2
         ]
     }
 ]
